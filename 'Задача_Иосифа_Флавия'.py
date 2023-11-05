e2789af50cc948c8ea984dbe111cf072928b56a0
griboris <126003941+griboris@users.noreply.github.com>
2023-11-05 16:38:29 +0300

    Update 'Задача_Иосифа_Флавия'.py

--- 'Задача_Иосифа_Флавия'.py
+++ 'Задача_Иосифа_Флавия'.py
@@ -13,3 +13,22 @@ while len(ones) > 1:
     i = i-len(ones)
     
 print(*ones)
+
+n = int(input())
+k = int(input())
+ 
+res = 0
+for i in range(1, n+1):
+    res = (res + k) % i
+print(res + 1)
+
+n = int(input())
+k = int(input())
+list = [i for i in range(1, n + 1)]
+
+while len(list) != 1:
+    for i in range(0, k-1):
+        list.append(list[i])
+    del list[0:k]
+
+print(*list)
